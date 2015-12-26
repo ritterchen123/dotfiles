2670fbfa0256c4bab65427d90659b40b1da61466
Simon Selg <simon@simonselg.me>
2015-12-27 00:59:37 +0100

    first customisation

--- brew-cask.sh
+++ brew-cask.sh
@@ -17,36 +17,30 @@ brew cask install gyazo
 brew cask install onepassword
 brew cask install rescuetime
 brew cask install flux
+brew cask install nosleep
+brew cask install telegram
+brew cask install spotify
 
 # dev
-brew cask install iterm2
+#brew cask install iterm2
+brew cask install iterm2-nightly
 brew cask install sublime-text
 brew cask install imagealpha
 brew cask install imageoptim
-
-# fun
-brew cask install limechat
-brew cask install miro-video-converter
-brew cask install horndis               # usb tethering
 
 # browsers
 brew cask install google-chrome-canary
 brew cask install firefox-nightly
 brew cask install webkit-nightly
-brew cask install chromium
-brew cask install torbrowser
 
 # less often
 brew cask install disk-inventory-x
-brew cask install screenflow4 # 4 specifically not 5.
 brew cask install vlc
-brew cask install gpgtools
 brew cask install licecap
 brew cask install utorrent
 
 
 # Not on cask but I want regardless.
 
-# 3Hub   https://itunes.apple.com/us/app/3hub/id427515976?mt=12 
 # File Multi Tool 5
-# Phosphor
+# Wunderlist
