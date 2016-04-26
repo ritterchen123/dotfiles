3e4f760b2d4806e7fb1f68d8d54e22bbe98ac588
Simon Selg <simon@selg.me>
2016-04-26 16:10:12 -0700

    feat(setup): remove things i don't use

--- setup-a-new-machine.sh
+++ setup-a-new-machine.sh
@@ -12,7 +12,6 @@ cd ~/migration
 
 # what is worth reinstalling?
 brew leaves      		> brew-list.txt    # all top-level brew installs
-brew cask list 			> cask-list.txt
 npm list -g --depth=0 	> npm-g-list.txt
 
 
@@ -25,14 +24,8 @@ cp ~/.extra ~/migration/home
 cp ~/.z ~/migration/home
 
 cp -R ~/.ssh ~/migration/home
-cp -R ~/.gnupg ~/migration/home
 
 cp /Library/Preferences/SystemConfiguration/com.apple.airport.preferences.plist ~/migration  # wifi
-
-cp ~/Library/Preferences/net.limechat.LimeChat.plist ~/migration
-cp ~/Library/Preferences/com.tinyspeck.slackmacgap.plist ~/migration
-
-cp -R ~/Library/Services ~/migration # automator stuff
 
 cp -R ~/Documents ~/migration
 
@@ -42,23 +35,11 @@ cp ~/.gitconfig.local ~/migration
 
 cp ~/.z ~/migration # z history file.
 
-# sublime text settings
-cp "~/Library/Application Support/Sublime Text 3/Packages" ~/migration
-
-
 # iTerm settings.
   # Prefs, General, Use settings from Folder
 
 # Finder settings and TotalFinder settings
 #   Not sure how to do this yet. Really want to.
-
-# Timestats chrome extension stats
-#   chrome-extension://ejifodhjoeeenihgfpjijjmpomaphmah/options.html#_options
-# 	gotta export into JSON through devtools:
-#     copy(JSON.stringify(localStorage, null, '  '))
-#     pbpaste > timestats-canary.json.txt
-
-# Current Chrome tabs via OneTab
 
 # software licenses like sublimetext
 
@@ -110,19 +91,13 @@ fi
 
 
 ##############################################################################################################
-### homebrew!
-
-# (if your machine has /usr/local locked down (like google's), you can do this to place everything in ~/.homebrew
-mkdir $HOME/.homebrew && curl -L https://github.com/mxcl/homebrew/tarball/master | tar xz --strip 1 -C $HOME/.homebrew
-export PATH=$HOME/.homebrew/bin:$HOME/.homebrew/sbin:$PATH
+### homebrew
 
 # install all the things
 ./brew.sh
-./brew-cask.sh
 
 ### end of homebrew
 ##############################################################################################################
-
 
 
 
@@ -151,15 +126,6 @@ git clone https://github.com/rupa/z.git ~/code/z
 # disable itunes opening on media keys
 git clone https://github.com/thebitguru/play-button-itunes-patch ~/code/play-button-itunes-patch
 
-
-# my magic photobooth symlink -> dropbox. I love it.
-# 	 + first move Photo Booth folder out of Pictures
-# 	 + then start Photo Booth. It'll ask where to put the library.
-# 	 + put it in Dropbox/public
-# 	* Now… you can record photobooth videos quickly and they upload to dropbox DURING RECORDING
-# 	* then you grab public URL and send off your video message in a heartbeat.
-
-
 # for the c alias (syntax highlighted cat)
 sudo easy_install Pygments
 
@@ -175,10 +141,6 @@ echo $BASH_VERSION # should be 4.x not the old 3.2.X
 
 # iterm with more margin! http://hackr.it/articles/prettier-gutter-in-iterm-2/
 #   (admittedly not as easy to maintain)
-
-
-# setting up the sublime symlink
-ln -sf "/Applications/Sublime Text.app/Contents/SharedSupport/bin/subl" ~/bin/subl
 
 
 ###
